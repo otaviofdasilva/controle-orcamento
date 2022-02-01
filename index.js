91393ccd79f5fb4317008939ac1b5d3c3cfc0f6d
otavio <95221817+otaviofdasilva@users.noreply.github.com>
2022-01-31 23:37:09 -0300

    permitido acesso de qualquer origem

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (_, response, next) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.removeHeader("x-powered-by");
+    next();
+});
+
 routes(app);
 
 app.listen(3000, function () {
